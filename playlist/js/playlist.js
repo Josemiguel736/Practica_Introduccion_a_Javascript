278e33cc837d187fdede2b2b8e83aec4ce47d754
Jose Miguel Atienzar <pcpirycjosemiguelatienzar@gmail.com>
2024-09-17 16:40:48 +0200

    Metodo createPlaylist creada

--- playlist/js/playlist.js
+++ playlist/js/playlist.js
@@ -27,7 +27,12 @@ const musicCatalog = () => {
    * Adds a new playlist to the catalog.
    * @param {string} playlistName - The name of the new playlist.
    */
-  const createPlaylist = (playlistName) => {};
+  const createPlaylist = (playlistName) => {
+    const playlist = {
+      name: playlistName,
+      songList: []
+    }; playlists.push(playlist)
+    };
 
   /**
    * Gets all playlists in the catalog.
