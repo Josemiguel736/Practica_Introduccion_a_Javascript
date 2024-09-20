e9f29da99c3babcc8320d7923c7735fae3cb44aa
Jose Miguel Atienzar <pcpirycjosemiguelatienzar@gmail.com>
2024-09-20 22:39:12 +0200

    Modificación ejercicio 1

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,7 +1,7 @@
 const usuario = {
 nombre : "Jose Miguel",
 apellidos : "Atiénzar Heras",
-temasBootcamp : [["Node.js",new Date("2024/10/21 19:00")],["React",new Date("2024/12/16 19:00")]]
+temasBootcamp : [["Node.js",new Date("2024/10/21 19:00")],["Git",new Date ("2024/05/24 19:00")],["React",new Date("2024/12/16 19:00")]]
 };
 console.log(usuario.temasBootcamp[0][1])
 
